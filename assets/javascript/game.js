179747fe6ba0f935f6ddfe07fee226854da7cfa7
Jia Fang <phoebe1002@gmail.com>
2018-08-23 23:51:48 -0700

    display underscores on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,14 @@ var currentWord;
 function reset() {
     var index = Math.floor(Math.random() * wordBank.length);
     currentWord = wordBank[index];
-    console.log(currentWord);
+    
+    //create underscores based on currentWord length
+    var underscores = [];
+    for(i = 0; i < currentWord.length; i++) {
+        underscores.push('_');
+    }
+
+    //display underscores
+    document.getElementById("display").innerHTML = underscores.join(" ");
 }
 
